830cfd9eae0fe5b7f4a81143cbef36f76da5d671
Ali <bad199614@gmail.com>
2020-03-18 21:56:48 +0500

    another day another commit

--- apito/settings.py
+++ apito/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@f*e5ry6&$t0(vci-=3-rysc%6_pepr_&2lrvsmiu9&yn2e=sn'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','apito.pythonanywhere.com','68.183.100.62','apebbleintheocean.com','www.apebbleintheocean.com','b835d74e.ngrok.io']
+ALLOWED_HOSTS = ['127.0.0.1','apito.pythonanywhere.com','68.183.100.62','apebbleintheocean.com','www.apebbleintheocean.com','192.168.8.101']
 
 
 # Application definition
